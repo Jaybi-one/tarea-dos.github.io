6b12b8a6ac3e982aa7b24fcfa4b4e55985ddd1a7
Jaybi-one <103076157+Jaybi-one@users.noreply.github.com>
2022-09-21 00:35:12 -0300

    Add files via upload

---
+++ assets/js/archivo.js
@@ -0,0 +1,43 @@
+$(document).ready(function(){
+    $(".cuadro-naranjo").click(function(){
+        $(this).toggleClass("naranjo")
+       
+    })
+})
+
+$(document).ready(function(){
+    $(".cuadro-morado-oscuro").click(function(){
+        $(this).toggleClass("morado-oscuro")
+       
+    })
+});
+
+$(document).ready(function(){
+    $(".cuadro-morado").click(function(){
+        $(this).toggleClass("morado")
+       
+    })
+});
+
+$(document).ready(function(){
+    $(".cuadro-verde").click(function(){
+        $(this).toggleClass("verde")
+       
+    })
+});
+
+$(document).ready(function(){
+    $(".cuadro-rosado").click(function(){
+        $(this).toggleClass("rosado")
+       
+    })
+});
+
+$(document).ready(function(){
+    $(".cuadro-gris").click(function(){
+        $(this).toggleClass("gris")
+       
+    })
+});
+
+
